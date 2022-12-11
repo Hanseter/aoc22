516d77243f3cb7b82a5d0103974d18fb47ae4f35
Hanseter <you@example.com>
2022-12-11 19:03:00 +0100

    Finish task 10

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import qualified Day09 (part1, part2)
+import qualified Day10 (part1, part2)
 import Data.Char (ord)
 
 main :: IO ()
 main = do
-  file <- readFile "/home/rene/projects/haskell/adventOfCode/res/day09.txt"
+  file <- readFile "/home/rene/projects/haskell/adventOfCode/res/day10.txt"
   let input = lines file
   putStrLn "Part 1"
-  putStrLn $ Day09.part1 input
+  putStrLn $ Day10.part1 input
   putStrLn "Part 2"
-  putStrLn $ Day09.part2 input
+  putStrLn $ Day10.part2 input
--- src/Day10.hs
+++ src/Day10.hs
@@ -1,10 +1,51 @@
 module Day10 (part1, part2) where
 
 import Data.String
+import Data.List
+import Data.Type.Coercion (sym)
+
+data Instruction = Instruction Int Int deriving (Show)
 
 part1 :: [String] -> String
-part1 lines = head lines
+part1 lines = show $sum $ analyzeSignalStrength 20 $ performInstructions 1 $ parseInstructions lines
 
+analyzeSignalStrength :: Int -> [Int] -> [Int]
+analyzeSignalStrength n signals = analyzeSignalStrength' n (drop (n -1) signals)
+
+analyzeSignalStrength' :: Int -> [Int] -> [Int]
+analyzeSignalStrength' n [] = []
+analyzeSignalStrength' n (x : xs) =
+  let strength = x * n
+   in strength : analyzeSignalStrength' (n + 40) (drop 39 xs)
+
+performInstructions :: Int -> [Instruction] -> [Int]
+performInstructions register [] = []
+performInstructions register ((Instruction 1 n) : xs) = register : performInstructions (n + register) xs
+performInstructions register ((Instruction count n) : xs) = register : performInstructions register ((Instruction (count -1) n) : xs)
+
+parseInstructions :: [String] -> [Instruction]
+parseInstructions = map parseInstruction
+
+parseInstruction :: String -> Instruction
+parseInstruction "noop" = Instruction 1 0
+parseInstruction line = Instruction 2 (read (drop 5 line))
 
 part2 :: [String] -> String
-part2 lines = head lines
+part2 lines = show $ evaluateSignal $ performInstructions 1 $ parseInstructions lines
+
+evaluateSignal:: [Int] -> [String]
+evaluateSignal [] = []
+evaluateSignal signal = map (evaluateLine 0) $ chunksOf 40 signal
+
+evaluateLine:: Int -> [Int] -> String
+evaluateLine _ [] = []
+evaluateLine n (current:rest) = let
+    symbol = if n == current || n == (current -1) || n == current + 1 then '#' else '.'
+    in symbol:evaluateLine (n+1) rest
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n = foldr (\v a ->
+    case a of
+        (x:xs) -> if length x < n then (v:x):xs else [v]:a
+        _ -> [[v]]
+    ) []
